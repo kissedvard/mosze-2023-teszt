b097313e377e8a47749f0229dd0c82ffe7190b1f
Kiss Edvárd <kissedvard22@gmail.com>
2026-03-03 20:57:42 +0100

    Kommentek torlese

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[N_ELEMENTS]; // Hibas nev
-    std::cout << '1-100 ertekek duplazasa'; // Pontos vesszo hianyzik
-    for (int i = 0; i < N_ELEMENTS; i++) // Hianyoznak parameterek
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << '1-100 ertekek duplazasa'; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i < N_ELEMENTS; i++) // Vegtelen ciklus lesz
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
         std::cout << "Ertek:";
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    double atlag = 0; // Nincs inicializalva a valtozo
+    double atlag = 0; 
     for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]; // Hianyzik a pontos vesszo 
+        atlag += b[i];  
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
